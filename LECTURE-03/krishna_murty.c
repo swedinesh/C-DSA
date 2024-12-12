3a7f2337b433bb227b6036e849bec733f9307977
swedinesh <137271989+swedinesh@users.noreply.github.com>
2024-12-12 23:03:02 +0530

    Loop
    Krishna Murty update.....

--- LECTURE-03/krishna_murty.c
+++ LECTURE-03/krishna_murty.c
@@ -1,3 +1,36 @@
+
+// KRISHNA MURTY FUNCTION EX 145 
+#include<stdio.h>
+
+int main() {
+    int i,n,fact,rem,original,sum=0;
+
+    printf("Enter a number :");
+    scanf("%d",&n);
+
+    original=n;
+
+    for( ; n != 0; ){
+        rem = n % 10;
+         fact = 1;
+        for( i=1; i<=rem; i++){
+            fact*=i;
+        }
+        sum += fact;
+        n /= 10;
+    }
+
+if(sum == original){
+    printf("%d number is Krishna Murty Good..!",original);
+}
+else{
+    printf("%d number is not Krishna Murty Ohh no...!",original);   
+}
+return 0;
+}
+
+/* 
+
 #include <stdio.h>
 
 int main() {
@@ -31,63 +64,16 @@ int main() {
         number /= 10;
     }
 
-    // Check if the sum of factorials is equal to the original number
+// Check if the sum of factorials is equal to the original number
     if (sum == original) {
         printf("%d is a Krishna Murthy number.\n", original);
-    } else {
+    }
+     else {
         printf("%d is not a Krishna Murthy number.\n", original);
     }
 
     return 0;
-}
-
-
-
-/* 
-#include <stdio.h>
-
-// Function to calculate the factorial of a number
-int factorial(int n) {
-    int fact = 1;
-    for (int i = 1; i <= n; i++) {
-        fact *= i;
-    }
-    return fact;
-}
-
-// Function to reverse a number
-int reverse(int n) {
-    int rev = 0;
-    while (n > 0) {
-        rev = rev * 10 + n % 10;
-        n /= 10;
-    }
-    return rev;
-}
-
-// Function to check if a number is a Krishna Murthy number
-int isKrishnaMurthy(int n) {
-    int rev = reverse(n);
-    int sum = 0, temp = rev;
-    while (temp > 0) {
-        sum += factorial(temp % 10);
-        temp /= 10;
-    }
-    return sum == n;
-}
-
-int main() {
-    int number;
-    printf("Enter a number: ");
-    scanf("%d", &number);
-
-    if (isKrishnaMurthy(number)) {
-        printf("%d is a Krishna Murthy number.\n", number);
-    } else {
-        printf("%d is not a Krishna Murthy number.\n", number);
-    }
-
-    return 0;
+    
 }
 
 */
